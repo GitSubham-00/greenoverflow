7bcb29b4e3ebd8d80efdfd8e970ca3be1b2ab239
subham <subhammaity700@gmail.com>
2025-04-24 22:32:37 +0530

    remove vite

--- src/main.js
+++
@@ -1,9 +0,0 @@
-import './style.css'
-import { setupCounter } from './counter.js'
-
-document.querySelector('#app').innerHTML = `
-  <div>
-  </div>
-`
-
-setupCounter(document.querySelector('#counter'))
